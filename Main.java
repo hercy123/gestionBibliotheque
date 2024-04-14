d46022e811b9fd7416ec8be38525ca358f7d3f2f
BRAHIM-coder <132566889+BRAHIM-coder@users.noreply.github.com>
2024-04-14 21:03:31 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
 import java.util.Scanner;
 
 public class Main {
-
+    /*
+        -permet de récupérer l'instance d'un utilisateur enregistré au sein du Système de gestion de la bibliothèque 
+        qui désire éffectuer une recherche en vue de faire un emprunt cela permet de vérifier est ce que l'utilisateur est un abonné de la bibliothèque 
+        et par conséquent s'il a le droit de faire un emprunt.
+        -permet également de récuperer l'instance d'un utilisateur pour vérifier s'il est apte à emprunter un livre de par sa cotisation.
+    */
     private static Utilisateur recupererUtilisateur(Bibliotheque bibliotheque, Scanner scanner){
         int numero;
         if (bibliotheque.getListUtilisateurs().isEmpty()) {
@@ -15,14 +20,21 @@ public class Main {
         } while (numero > bibliotheque.getListUtilisateurs().size() || numero <= 0);
         return bibliotheque.getListUtilisateurs().get(numero - 1);
     }
-
+    /*
+        gère l'emprunt d'un utilisateur au sein de la bibliothèque du moment ou l'utilisateur initie l'emprunt
+        jusqu'au  moment ou le système enregistre l'emprunt si l'utilisateur a payé
+        sa cotisation. 
+    */
     private static void gererAjoutEmprunt(Bibliotheque bibliotheque){
         Scanner sc = new Scanner(System.in);
         Utilisateur utilisateur = recupererUtilisateur(bibliotheque, sc);
         if (utilisateur == null ) return;
         utilisateur.emprunterLivre(bibliotheque);
     }
-    
+    /*
+        gère le retour d'un livre éffectué par un utilisateur au sein de la bibliothèque du moment ou l'utilisateur initie le retour
+        jusq'au moment ou le système enregistre le retour.
+    */
     private static void gererRetourLivre(Bibliotheque bibliotheque){
         Scanner scanner = new Scanner(System.in);
         Utilisateur utilisateur = recupererUtilisateur(bibliotheque, scanner);
@@ -30,6 +42,11 @@ public class Main {
         utilisateur.retournerLivre(bibliotheque);
     }
 
+    /*
+        cette méthode comme son nom l'indique permet de néttoyer l'écran de la console
+        en faisant appel à une commande en fonction du système d'exploitation.
+        
+    */
     public static void nettoyerConsole() {
         try {
             String systemeExploitation = System.getProperty("os.name").toLowerCase();
@@ -46,8 +63,8 @@ public class Main {
 
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        int choix;
-        String retourAuMenuPrincipale,retourAuMenu;
+        int choix; //permet de faire un choix par rapport aux options du menu principale ou aux options des autres menus de Gestion.
+        String retourAuMenuPrincipale,retourAuMenu;//permet de  definir le retour à un menu aussi bien au menu principal que les autres menu de gestion.
         Bibliotheque bibliotheque = new Bibliotheque();
             do{ 
                 retourAuMenuPrincipale = null;
@@ -155,6 +172,7 @@ public class Main {
 
                                 switch (choix) {
                                     case 1:
+                                    //permet d'enregistrer les emprunts par un utilisateur.
                                         if (bibliotheque.getListeLivres().isEmpty()) {
                                             System.out.println("il n'y a aucun livre dans la bibliothèque Veuillez les ajouter et Réessayer.");
                                             retourAuMenuPrincipale = "oui";
@@ -174,6 +192,7 @@ public class Main {
                                         }
                                         break;
                                     case 2:
+                                    //permet d'enregistrer le retour des livres
                                         if (bibliotheque.getEmpruntsUtilisateurs().isEmpty()) {
                                             System.out.println("impossible d'enregistrer un retour car il n'y aucun emprunt en cours.");
                                             retourAuMenuPrincipale = "oui";
@@ -190,6 +209,7 @@ public class Main {
                                         }
                                         break;
                                     case 3:
+                                    //affiche les livres emprunté par les utilisateurs
                                         if (bibliotheque.getEmpruntsUtilisateurs().isEmpty()) {
                                             System.out.println("désole aucun utilisateur n'a un emprunt en cours.");
                                             retourAuMenuPrincipale = "oui";
@@ -199,6 +219,7 @@ public class Main {
                                         }
                                         break;
                                     case 4:
+                                        //permet de limiter le nombre de livre emprunter au  sein de la bibliotheque.
                                         System.out.println("LIMITATION DE NOMBRE DE LIVRES EMPRUNTER");
                                         System.out.println("La limite actuelle est de "+bibliotheque.getMaxEmprunt()+" livre a la fois");
                                         System.out.println("Veuillez saisir la nouvelle limite");
@@ -243,6 +264,7 @@ public class Main {
                                         bibliotheque.enregistrerUtilisateur();
                                         break;
                                     case 2:
+                                        //vérifie qu'un utilisateur est apte à emprunter un livre si il a versé sa cotisation.
                                         if (bibliotheque.getListUtilisateurs().isEmpty()) {
                                             System.out.println("Il n y a aucun utilisateur dans la bibliothèque Veuillez les ajouter et Réessayer.");
                                             retourAuMenuPrincipale = "oui";
