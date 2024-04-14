271a12e9d08bf3d8048404cecbf088dd77caac43
BRAHIM-coder <132566889+BRAHIM-coder@users.noreply.github.com>
2024-04-14 20:23:58 +0000

    Update Utilisateur.java

--- Utilisateur.java
+++ Utilisateur.java
@@ -48,25 +48,25 @@ public class Utilisateur {
     
 // cette méthode nous permet d'emprunter un livre
     public void emprunterLivre(Bibliotheque bibliotheque) {
-        Livre livre = bibliotheque.rechercherLivre();
+        Livre livre = bibliotheque.rechercherLivre(); // cette méthode permet de rechercher un livre disponible dans la bibliotheque et elle retourne un livre, si un livre disponible est trouvé, sinon elle retourne null 
         if (livre != null) {
             livre = new Livre(livre);
             livre.setQuantite(1);
-            bibliotheque.enregistrerEmpruntLivre(this, livre);
+            bibliotheque.enregistrerEmpruntLivre(this, livre); // cette methode permet d'enregistrer l'emprunt du livre 
         }
        
     }
     
-// cette méthode nous permet de retourner un livre qui a été emprunté
+// cette méthode nous permet de retourner un livre qui a été emprunté 
     public void retournerLivre(Bibliotheque bibliotheque) {
         Scanner scanner = new Scanner(System.in);
         int numero;
-        if (this.livresEmpruntes.isEmpty()) {
+        if (this.livresEmpruntes.isEmpty()) { // permet de verifier si l'utilisateur si l'utilisateur a emprunté des livres, si la liste est vide elle affiche un message a l'utilisateur "n'a aucun emprunt en cours" 
             System.out.println(this.nom+" n'a aucun emprunt en cours");
             return;
         }
-        System.out.println("Veuillez indiquer le livre a retourner");
-        this.afficherLivreEmpruntee();
+        System.out.println("Veuillez indiquer le livre a retourner"); 
+        this.afficherLivreEmpruntee();// cette methode permet d'afficcher les livres empruntés par l'utilisateur
         do {
             System.out.print("Numero du Livre: ");
             numero = scanner.nextInt();
